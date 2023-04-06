d844cefa5c547f8dbda71765fb46bbcba40abdef
hariprasad154 <hariprasas154@gmail.com>
2023-04-06 13:22:52 +0530

    added Succesfully

--- src/main/java/LoginServelet.java
+++ src/main/java/LoginServelet.java
@@ -1,5 +1,6 @@
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,28 +8,52 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-@WebServlet("/LoginServlet")
+
+
+@WebServlet(description = "Login Servlet Testing",
+        urlPatterns = {"/LoginServlet"},
+        initParams = {
+            @WebInitParam(name="user",value = "Admin"),
+             @WebInitParam(name="pwd",value = "Pass")
+        }
+        )
 public class LoginServelet extends HttpServlet {
     protected void doPost(HttpServletRequest req , HttpServletResponse res) throws IOException, ServletException {
-        String userName=req.getParameter("user");
+        String user=req.getParameter("user");
         String password=req.getParameter("password");
 
-        if(userName.equals("admin") && password.equals("password")){
-            HttpSession session=req.getSession();
-            session.setAttribute(userName,userName);;
-            try {
-                res.sendRedirect("success.jsp");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+        String userId=getServletConfig().getInitParameter("user");
+        String pwd=getServletConfig().getInitParameter("pwd");
 
-        }else{
+        Pattern userPattern = Pattern.compile("^[A-Z]+[a-z]{3,}$");
+         Matcher UserMatcher = userPattern.matcher(user);
+
+        if (UserMatcher.matches()) {
+            if(userId.equals(user) && pwd.equals(password)){
+                HttpSession session=req.getSession();
+                session.setAttribute("user",user);
+
+                try {
+                    req.getRequestDispatcher("success.jsp").forward(req ,res);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }else{
+                RequestDispatcher rd = getServletContext().getRequestDispatcher("/Login.html");
+                PrintWriter out = res.getWriter();
+                out.println("<font color=red> Incorrect credentials </font>");
+                rd.include(req,res);
+
+            }
+        }else {
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/Login.html");
             PrintWriter out = res.getWriter();
-            out.println("<font color=red> Incorrect credentials </font>");
+            out.println("<font color=red> Use correct naming coventions </font>");
             rd.include(req,res);
-
         }
     }
 }
