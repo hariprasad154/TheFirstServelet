77468ef341bfb24df3cbdfad4f63e4cdfa4be60c
hariprasad154 <hariprasas154@gmail.com>
2023-04-06 09:22:52 +0530

    The Servelet Has been Created

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -1,5 +1,13 @@
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class demo {
 
+@WebServlet("/home")
+public class demo extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res){
+        res.setContentType("text/html");
+        res.getWriter().println("<h1>Hello World</h1>");
+    }
 }
