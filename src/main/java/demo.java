a2b23c2d7ed9c7b8d68637848fa3749fa6d1ccc4
hariprasad154 <hariprasas154@gmail.com>
2023-04-06 11:00:58 +0530

    added Succesfully

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -2,12 +2,17 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
-@WebServlet("/home")
+@WebServlet("/hello")
 public class demo extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res){
         res.setContentType("text/html");
-        res.getWriter().println("<h1>Hello World</h1>");
+        try {
+            res.getWriter().println("<h1>Java Home</h1>");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
